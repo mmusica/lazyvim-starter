d873f7d73f773d499d5d72386d4f0ce5fe505bc5
Marko Musica <markomusica@gmail.com>
2026-01-05 17:53:21 +0100

    Sonarlint and oil config

--- lua/plugins/sonarlint.lua
+++ lua/plugins/sonarlint.lua
@@ -1,22 +1,27 @@
--- require("sonarlint").setup({
---   server = {
---     cmd = {
---       "sonarlint-language-server",
---       -- Ensure that sonarlint-language-server uses stdio channel
---       "-stdio",
---       "-analyzers",
---       -- paths to the analyzers you need, using those for python and java in this example
---       vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
---       vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
---       vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
---     },
---   },
---   filetypes = {
---     -- Tested and working
---     "cs",
---     "dockerfile",
---     "python",
---     "cpp",
---     "java",
---   },
--- })
+return {
+  "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+  dependencies = {
+    "mason-org/mason.nvim",
+  },
+  config = function()
+    local mason_path = vim.fn.stdpath("data") .. "/mason/"
+
+    require("sonarlint").setup({
+      server = {
+        cmd = {
+          "sonarlint-language-server",
+          "-stdio",
+          "-analyzers",
+          -- Adjust these paths based on what you actually install in Mason
+          vim.fn.expand(mason_path .. "share/sonarlint-analyzers/sonarpython.jar"),
+          vim.fn.expand(mason_path .. "share/sonarlint-analyzers/sonarcfamily.jar"),
+          vim.fn.expand(mason_path .. "share/sonarlint-analyzers/sonarjava.jar"),
+        },
+      },
+      filetypes = {
+        "python",
+        "cpp",
+      },
+    })
+  end,
+}
