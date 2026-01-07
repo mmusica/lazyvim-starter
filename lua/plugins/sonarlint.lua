3f17de1d1c13d90e38fc6a47c7bc0e82dc225116
Marko Musica <markomusica@gmail.com>
2026-01-07 14:10:33 +0100

    Sonarlint config

--- lua/plugins/sonarlint.lua
+++ lua/plugins/sonarlint.lua
@@ -2,6 +2,7 @@ return {
   "https://gitlab.com/schrieveslaach/sonarlint.nvim",
   dependencies = {
     "mason-org/mason.nvim",
+    "mfussenegger/nvim-jdtls",
   },
   config = function()
     local mason_path = vim.fn.stdpath("data") .. "/mason/"
@@ -12,15 +13,19 @@ return {
           "sonarlint-language-server",
           "-stdio",
           "-analyzers",
-          -- Adjust these paths based on what you actually install in Mason
+
           vim.fn.expand(mason_path .. "share/sonarlint-analyzers/sonarpython.jar"),
           vim.fn.expand(mason_path .. "share/sonarlint-analyzers/sonarcfamily.jar"),
           vim.fn.expand(mason_path .. "share/sonarlint-analyzers/sonarjava.jar"),
+          vim.fn.expand(mason_path .. "share/sonarlint-analyzers/sonarjs.jar"),
         },
       },
       filetypes = {
         "python",
         "cpp",
+        "java",
+        "js",
+        "ts",
       },
     })
   end,
